3c5d9f559b409608b2205a1b7efc96096ac607d9
Anthony Catalano <Anthony.Catalano@its.ny.gov>
2022-08-15 16:07:56 -0400

    finished video 15

--- Course1/Bulleye/app/src/main/java/gov/ny/its/bulleye/MainActivity.kt
+++ Course1/Bulleye/app/src/main/java/gov/ny/its/bulleye/MainActivity.kt
@@ -2,6 +2,8 @@ package gov.ny.its.bulleye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import gov.ny.its.bulleye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+       binding.btnHitMe.setOnClickListener {
+           Log.i("Button Click Event", "You clicked the hit me button")
+      showResults()
+       }
+
+
     }
+    private fun showResults(){
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = getString(R.string.result_dialog_message)
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.btnHitMe_text) {dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.create().show()
+    }
+
 }
